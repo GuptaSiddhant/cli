01fb87890095abc19a1ad38b0f3e4d9c4582b3b4
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2021-11-28 22:17:10 +0200

    Update publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,19 +3,23 @@ const { readFileSync, writeFileSync } = require("fs");
 
 const token = process.argv[2];
 const manifestPath = "package.json";
+const preId = "canary";
 
-(async () => {
-  const result = await npmPublish({ token });
+npmPublish({ token })
+  .then((type) => {
+    if (type === "none") {
+      const manifestFile = readFileSync(manifestPath, "utf8");
+      const manifest = JSON.parse(manifestFile);
+      const canaryManifest = {
+        ...manifest,
+        version: `${manifest.version}-${preId}.${Date.now().valueOf()}`,
+      };
+      writeFileSync(manifestPath, JSON.stringify(canaryManifest, null, 2));
 
-  // If did not publish -> publish canary
-  if (result.type === "none") {
-    const manifestFile = readFileSync(manifestPath, "utf8");
-    const manifest = JSON.parse(manifestFile);
-    const canaryManifest = {
-      ...manifest,
-      version: `${manifest.version}-canary.${Date.now().valueOf()}`,
-    };
-    writeFileSync(manifestPath, JSON.stringify(canaryManifest, null, 2));
-    await npmPublish({ token, tag: "canary" });
-  }
-})();
+      return npmPublish({ token, tag: preId });
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
